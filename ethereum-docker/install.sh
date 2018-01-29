9d36ae29441397275cbfa26131a745e094c9f7d5
Steve Gehrman <sgehrman@gmail.com>
2018-01-29 00:30:11 -0800

    sdf

--- ethereum-docker/install.sh
+++ ethereum-docker/install.sh
@@ -23,7 +23,7 @@ else
   curl $ETHEREUM_ASC_URL -o $ETHEREUM_FILE_NAME.asc
 
   # check signature
-  gpg --recv-keys F9585DE6 C2FF8BBF 9BA28146 7B9E2481 D2A67EAC
+  gpg --keyserver=pgp.mit.edu --recv-keys F9585DE6 C2FF8BBF 9BA28146 7B9E2481 D2A67EAC
 
   # result is in 'result code' at $?
   gpg --verify geth-linux-amd64-1.7.3-4bb3c89d.tar.gz.asc
